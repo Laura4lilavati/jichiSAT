1f29e15e9766a253eac11f0873a9214748fa5dcb
Pedro Lima <pedrolc05@gmail.com>
2022-07-03 20:15:41 -0400

    Fix constuctor not needed on Icomunication

--- include/IComunication.h
+++ include/IComunication.h
@@ -1,11 +1,6 @@
 
 class IComunication{
-private:
-  IComunication();
 public:
-  IComunication(){
-    //built the infrastucture for com like wifi.begin();
-  }
   virtual void sendData(int* jsonData) = 0; //int is just a way to compile for now
   //please change that INT!!!!
   virtual int* receiveData() = 0;
