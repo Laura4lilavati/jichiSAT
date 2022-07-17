3a5c5f4d03302aa0b639196a948f0d87e2690b36
Pedro Lima <pedrolc05@gmail.com>
2022-07-17 14:53:39 -0400

    Add some dummy logic/code on callback lambda

--- include/wifiComunication.hpp
+++ include/wifiComunication.hpp
@@ -34,33 +34,29 @@ public:
     auto callback = [&wCom] (char* topic, byte* payload, unsigned int length) {
       int a = 77;
       wCom->myData = &a;
+      Serial.print ("Message arrived [");
+      Serial.print (topic);
+      Serial.print ("] ");
+      //instead of this bellow make a jsonData
+      for (unsigned int i = 0; i < length; i++) {
+        Serial.print ((char)payload[i]);
+      }
+      Serial.println();
+      // Switch on the LED if an 1 was received as first character
+      // INSTEAD change this to recieve an int for servo's position
+      if ((char)payload[0] == '1') {
+        digitalWrite (LED_BUILTIN, LOW);  // Turn the LED on (Note that LOW is the voltage level
+        // but actually the LED is on; this is because
+        // it is active low on the ESP-01)
+      } else {
+        digitalWrite (LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH
+      }
     };
     wCom->myMqttClient.setCallback(callback);
   }
 
   int* receiveData() override {
     return myData;
-  }
-
-  void callback1 (char* topic, byte* payload, unsigned int length)
-  {
-    Serial.print ("Message arrived [");
-    Serial.print (topic);
-    Serial.print ("] ");
-    //instead of this bellow make a jsonData
-    for (unsigned int i = 0; i < length; i++) {
-      Serial.print ((char)payload[i]);
-    }
-    Serial.println();
-    // Switch on the LED if an 1 was received as first character
-    // INSTEAD change this to recieve an int for servo's position
-    if ((char)payload[0] == '1') {
-      digitalWrite (LED_BUILTIN, LOW);  // Turn the LED on (Note that LOW is the voltage level
-      // but actually the LED is on; this is because
-      // it is active low on the ESP-01)
-    } else {
-      digitalWrite (LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH
-    }
   }
 
   wifiComunication (const char* clientID): mqttClientID(clientID){
